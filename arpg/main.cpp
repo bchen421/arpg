a52df4d2cb8b3b04d025e0314093afc7eab86071
bchen <benjamin.chen@mlb.com>
2015-06-05 18:46:08 -0400

    Initial Commit

---
+++ arpg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arpg
+//
+//  Created by Benjamin Chen on 6/5/15.
+//  Copyright (c) 2015 chenjamin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
